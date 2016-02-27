8fda74d65c4346f936fab448b6a0e9e36c3f1d87
Alex Hoar <Alex Hoar>
2016-02-27 15:27:13 -0800

    Added necessary prerequisite lines to AndroidManifest.xml, and a MainActivity.onResume() function to process the NFC discovery,

--- app/src/main/java/com/nwhacks/superteam/nfckiosk/MainActivity.java
+++ app/src/main/java/com/nwhacks/superteam/nfckiosk/MainActivity.java
@@ -1,6 +1,10 @@
 package com.nwhacks.superteam.nfckiosk;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +13,20 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    NdefMessage[] msgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        intent = new Intent(this, MainActivity.class);
+        msgs = new NdefMessage[]{}; //initialize message array to be empty
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
@@ -48,5 +63,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+                String s = msgs[0].toString();
+            }
+        }
+        //process the msgs array
+
     }
 }
