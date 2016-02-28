26d4684a6195b55a139dc13b4b2730f4935110fb
eyqs <eugeneyqshen@gmail.com>
2016-02-27 16:59:51 -0800

    Add import statements

--- app/src/main/java/com/nwhacks/superteam/nfckiosk/MainActivity.java
+++ app/src/main/java/com/nwhacks/superteam/nfckiosk/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nwhacks.superteam.nfckiosk;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
@@ -8,11 +9,10 @@ import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +40,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+        else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            else {
+                // do stuff we want to do
+            }
+        }
 
     }
 
