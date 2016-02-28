2ff20b9072662f02774e72cdb9c0bfe12b34020f
JeffreyDoyle <jeffreydoylec@gmail.com>
2016-02-27 18:12:12 -0800

    With some added NFC support.

--- app/src/main/java/com/nwhacks/superteam/nfckiosk/MainActivity.java
+++ app/src/main/java/com/nwhacks/superteam/nfckiosk/MainActivity.java
@@ -6,11 +6,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -20,18 +20,28 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.IntentFilter.MalformedMimeTypeException;
+
+import com.nwhacks.superteam.nfckiosk.NFC.NdefReaderTask;
+import com.nwhacks.superteam.nfckiosk.NFC.nfcHandler;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Intent intent;
     NdefMessage[] msgs;
+
+    //NFC Adaptor
+    private NfcAdapter mNfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
 
         intent = new Intent(this, MainActivity.class);
@@ -72,6 +82,16 @@ public class MainActivity extends AppCompatActivity {
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         mBluetoothAdapter.startDiscovery();
 
+
+        //Checking for NFC compatability
+        if (!mNfcAdapter.isEnabled()) {
+
+        } else {
+            print("NFC is working on this device.");
+        }
+
+        handleIntent(getIntent());
+
     }
 
     @Override
@@ -100,12 +120,16 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
 
+        setupForegroundDispatch(this, mNfcAdapter);
+
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
+        setupForegroundDispatch(this, mNfcAdapter);
 
     }
 
@@ -115,4 +139,93 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    /*
+    Makes sure the system lets this app actually handle an NFC instance.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+    /*
+    Stops the shit I did in the function above
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
+    /*
+    Function listens for new NFC event.
+    Contacts the nfcHandler and lets it deal with it.
+     */
+
+    private com.nwhacks.superteam.nfckiosk.NFC.nfcHandler nfcHandler;
+
+
+    /*
+    Handles when the device incounters an NFC signal.
+     */
+    @Override
+    public void onNewIntent(Intent intent){
+
+        //TODO: Actually make this work.
+
+        //Do something with the output
+
+    }
+
+
+    public void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask().execute(tag);
+                    break;
+                }
+            }
+        }
+
+
+
+    }
+
+
+
+
 }
